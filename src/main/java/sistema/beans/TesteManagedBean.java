43edcb02e7ca5934e3e3f6eab04670ef1055de38
Ariel Vitor Molina <Ariel Vitor Molina@172.18.1.36>
2018-03-06 22:33:01 -0300

    Tentando consertar.

---
+++ src/main/java/sistema/beans/TesteManagedBean.java
@@ -0,0 +1,19 @@
+package sistema.beans;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class TesteManagedBean {
+	/*
+	public void salvar()
+	{
+		service.salvar(aluno);
+		aluno = new Aluno();
+		
+	}
+	 */
+}
