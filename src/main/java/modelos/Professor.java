1d45ccf7b5b326cb7b483faef7dc307008990976
Ariel Vitor Molina <Ariel Vitor Molina@172.18.1.36>
2018-03-06 22:00:46 -0300

    Mudança para projeto inicial do Glauco.

---
+++ src/main/java/modelos/Professor.java
@@ -0,0 +1,28 @@
+package sistema.modelos;
+
+public class Professor {
+	//Atributos
+	private int registro;
+	private String nome;
+	private String curso;
+	
+	//Getters e Setters
+	public int getRegistro() {
+		return registro;
+	}
+	public void setRegistro(int registro) {
+		this.registro = registro;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+}
